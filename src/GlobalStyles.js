914ee4f51eaa5e402935d9376b69b8976dbd5f54
soyeon102 <csea9000@naver.com>
2022-08-19 18:30:00 +0900

    글로벌스타일 수정

--- src/GlobalStyles.js
+++ src/GlobalStyles.js
@@ -3,10 +3,6 @@ import reset from 'styled-reset';
 
 const GlobalStyles = createGlobalStyle` 
   ${reset}
-  body {
-    font-family: 'Noto Sans KR', sans-serif;    
-    line-height: 1.5;    
-  }
 `;
 
 export default GlobalStyles;
