914f5490e8c2a84d3e8764fccf24dafe7a313f8e
soyeon102 <csea9000@naver.com>
2022-08-21 03:56:07 +0900

    connected react router 연결 시도 중

--- src/index.js
+++ src/index.js
@@ -2,10 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { ConnectedRouter } from 'connected-react-router';
+import { store } from './redux/config/configStore';
+import { Provider } from 'react-redux';
+import history from './history';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <Provider store={store}>
+    <ConnectedRouter history={history}>
+      <App />
+    </ConnectedRouter>
+  </Provider>
 );
---
+++ src/redux/modules/userSlice.js
@@ -0,0 +1,89 @@
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
+import { connectRouter } from 'connected-react-router';
+// import history from '../../history';
+import axios from 'axios';
+import { getCookie, setCookie } from '../../shared/Cookie';
+
+const BASE_URL = 'http://13.209.97.60';
+
+const config = {
+  headers: {
+    Authorization: getCookie('ACCESS_TOKEN'),
+  },
+};
+
+export const __loginUser = createAsyncThunk(
+  'LOGIN_USER',
+  async (payload, thunkAPI) => {
+    try {
+      const data = await axios.post(`${BASE_URL}/api/member/login`, payload);
+      setCookie('ACCESS_TOKEN', data.headers.authorization);
+      return thunkAPI.fulfillWithValue(data.data);
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error);
+    }
+  }
+);
+
+export const __signupUser = createAsyncThunk(
+  'SIGNUP_USER',
+  async (payload, thunkAPI) => {
+    try {
+      const data = await axios.post(`${BASE_URL}/api/member/signup`, payload);
+      // history.push('/login');
+      return thunkAPI.fulfillWithValue(data.data);
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error.response.data.errorMessage);
+    }
+  }
+);
+
+const initialState = {
+  user: {},
+  isLogin: false,
+  error: null,
+};
+
+export const userSlice = createSlice({
+  name: 'users',
+  initialState,
+  reducers: {},
+  extraReducers: {
+    [__loginUser.pending]: (state) => {
+      state.isLoading = true;
+    },
+    [__loginUser.fulfilled]: (state, action) => {
+      state.isLoading = false;
+      state.user = action.payload;
+      state.isLogin = true;
+    },
+    [__loginUser.rejected]: (state, action) => {
+      state.isLoading = false;
+      state.error = action.payload;
+    },
+    [__signupUser.pending]: (state) => {
+      state.isLoading = true;
+    },
+    [__signupUser.fulfilled]: (state, action) => {
+      state.isLoading = false;
+      state.user = action.payload;
+    },
+    [__signupUser.rejected]: (state, action) => {
+      state.isLoading = false;
+      state.error = action.payload;
+    },
+    // [__getUser.pending]: (state) => {
+    //   state.isLoading = true;
+    // },
+    // [__getUser.fulfilled]: (state, action) => {
+    //   state.isLoading = false;
+    //   state.user = action.payload;
+    // },
+    // [__getUser.rejected]: (state, action) => {
+    //   state.isLoading = false;
+    //   state.error = action.payload;
+    // },
+  },
+});
+
+export default userSlice.reducer;
